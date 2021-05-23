dd83399ef5472b06ae9e9f911d6121e3277724d6
Shynggys Batyrbayev <Shynggys_Batyrbayev@epam.com>
2021-05-23 14:30:34 +0600

    feature-1: improves saving bpmn diagrams to json

--- client/src/pages/BpmnModelerPage.js
+++ client/src/pages/BpmnModelerPage.js
@@ -2,12 +2,10 @@ import React, {useEffect, useState} from 'react';
 import Modeler from "bpmn-js/lib/Modeler";
 import "bpmn-js/dist/assets/diagram-js.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
-import axios from "axios";
+const xml2js = require('xml2js');
 
 export function BpmnModelerPage() {
-  const [diagram, diagramSet] = useState('')
-  const [formattedDiagram, setFormattedDiagram] = useState('')
-  const [downloadLink, setDownloadLink] = useState('')
+  const [diagram, setDiagram] = useState('')
   const container = document.getElementById("container");
   const modeler = new Modeler({
     container,
@@ -18,16 +16,70 @@ export function BpmnModelerPage() {
 
   useEffect(() => {
     if (diagram.length === 0) {
-      axios
-        .get(
-          "https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/master/colors/resources/pizza-collaboration.bpmn"
-        )
-        .then((r) => {
-          diagramSet(r.data);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      const diagramXml = `<?xml version="1.0" encoding="UTF-8"?>
+      <bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:custom="http://custom/ns" id="Definitions_1" targetNamespace="http://bpmn.io/schema/bpmn">
+        <bpmn:process id="Process_1" isExecutable="false">
+          <bpmn:startEvent id="StartEvent_1">
+            <bpmn:outgoing>SequenceFlow_0b6cm13</bpmn:outgoing>
+          </bpmn:startEvent>
+          <bpmn:sequenceFlow id="SequenceFlow_0b6cm13" sourceRef="StartEvent_1" targetRef="Task_0zlv465" />
+          <bpmn:endEvent id="EndEvent_09arx8f">
+            <bpmn:incoming>SequenceFlow_035kn8o</bpmn:incoming>
+          </bpmn:endEvent>
+          <bpmn:sequenceFlow id="SequenceFlow_17w8608" sourceRef="Task_0zlv465" targetRef="Task_1xewseo" />
+          <bpmn:task id="Task_1xewseo" name="Do more work">
+            <bpmn:incoming>SequenceFlow_17w8608</bpmn:incoming>
+            <bpmn:outgoing>SequenceFlow_035kn8o</bpmn:outgoing>
+          </bpmn:task>
+          <bpmn:sequenceFlow id="SequenceFlow_035kn8o" sourceRef="Task_1xewseo" targetRef="EndEvent_09arx8f" />
+          <bpmn:serviceTask id="Task_0zlv465" name="Do work" custom:topic="my.custom.topic">
+            <bpmn:incoming>SequenceFlow_0b6cm13</bpmn:incoming>
+            <bpmn:outgoing>SequenceFlow_17w8608</bpmn:outgoing>
+          </bpmn:serviceTask>
+        </bpmn:process>
+        <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+          <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+            <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
+              <dc:Bounds x="173" y="188" width="36" height="36" />
+              <bpmndi:BPMNLabel>
+                <dc:Bounds x="146" y="224" width="90" height="20" />
+              </bpmndi:BPMNLabel>
+            </bpmndi:BPMNShape>
+            <bpmndi:BPMNEdge id="SequenceFlow_0b6cm13_di" bpmnElement="SequenceFlow_0b6cm13">
+              <di:waypoint x="209" y="206" />
+              <di:waypoint x="256" y="206" />
+              <bpmndi:BPMNLabel>
+                <dc:Bounds x="192.5" y="110" width="90" height="20" />
+              </bpmndi:BPMNLabel>
+            </bpmndi:BPMNEdge>
+            <bpmndi:BPMNShape id="EndEvent_09arx8f_di" bpmnElement="EndEvent_09arx8f">
+              <dc:Bounds x="552" y="188" width="36" height="36" />
+              <bpmndi:BPMNLabel>
+                <dc:Bounds x="404" y="138" width="90" height="20" />
+              </bpmndi:BPMNLabel>
+            </bpmndi:BPMNShape>
+            <bpmndi:BPMNEdge id="SequenceFlow_17w8608_di" bpmnElement="SequenceFlow_17w8608">
+              <di:waypoint x="356" y="206" />
+              <di:waypoint x="399" y="206" />
+              <bpmndi:BPMNLabel>
+                <dc:Bounds x="353.5" y="110" width="90" height="20" />
+              </bpmndi:BPMNLabel>
+            </bpmndi:BPMNEdge>
+            <bpmndi:BPMNShape id="Task_1xewseo_di" bpmnElement="Task_1xewseo">
+              <dc:Bounds x="399" y="166" width="100" height="80" />
+            </bpmndi:BPMNShape>
+            <bpmndi:BPMNEdge id="SequenceFlow_035kn8o_di" bpmnElement="SequenceFlow_035kn8o">
+              <di:waypoint x="499" y="206" />
+              <di:waypoint x="552" y="206" />
+            </bpmndi:BPMNEdge>
+            <bpmndi:BPMNShape id="ServiceTask_0wob562_di" bpmnElement="Task_0zlv465">
+              <dc:Bounds x="256" y="166" width="100" height="80" />
+            </bpmndi:BPMNShape>
+          </bpmndi:BPMNPlane>
+        </bpmndi:BPMNDiagram>
+      </bpmn:definitions>
+      `
+      setDiagram(diagramXml);
     }
   }, [diagram]);
 
@@ -52,7 +104,15 @@ export function BpmnModelerPage() {
 
   function saveXML() {
     modeler.saveXML().then(({ xml }) => {
-      download(xml, 'file.xml')
+      const { parseString } = xml2js;
+
+      parseString(xml, (err, json) => {
+        if (err) {
+          console.error(err);
+        }
+
+        download(JSON.stringify(json), 'file.txt')
+      });
     });
   }
 
@@ -89,7 +149,7 @@ export function BpmnModelerPage() {
         download='list.txt'
         onClick={saveXML}
       >
-        Download XML file
+        Download file
       </a>
     </div>
   );
